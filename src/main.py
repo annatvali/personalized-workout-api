f297d5d9109ffb6aea76227f9a16ffd509fd611b
annatvali <anntvaliashvili@gmail.com>
2025-02-02 23:57:35 +0400

    feat: include exercise routes and populate database

--- src/main.py
+++ src/main.py
@@ -1,7 +1,11 @@
 from fastapi import FastAPI
 
-from src.routes import auth
+from src.routes import auth, exercise
+from src.services.populate_exercises import populate_exercises
 
 app = FastAPI()
 
 app.include_router(auth.router)
+app.include_router(exercise.router)
+
+populate_exercises()
