d54262517703a9ad35a3d5728a5ed10a0083e74e
annatvali <anntvaliashvili@gmail.com>
2025-02-03 23:56:55 +0400

    chore: include tracking routes in main application

--- src/main.py
+++ src/main.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI
 
-from src.routes import auth, exercise, workout_plan
+from src.routes import auth, exercise, tracking, workout_plan
 from src.services.populate_exercises import populate_exercises
 
 app = FastAPI()
@@ -8,5 +8,6 @@ app = FastAPI()
 app.include_router(auth.router)
 app.include_router(exercise.router)
 app.include_router(workout_plan.router)
+app.include_router(tracking.router)
 
 populate_exercises()
