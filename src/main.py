d825796edfd88b401aa16f4c901a4aa5ed8f33bb
annatvali <anntvaliashvili@gmail.com>
2025-02-04 02:00:40 +0400

    chore: add workout session routes to main app

--- src/main.py
+++ src/main.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI
 
-from src.routes import auth, exercise, tracking, workout_plan
+from src.routes import auth, exercise, tracking, workout_plan, workout_session
 from src.services.populate_exercises import populate_exercises
 
 app = FastAPI()
@@ -9,5 +9,6 @@ app.include_router(auth.router)
 app.include_router(exercise.router)
 app.include_router(workout_plan.router)
 app.include_router(tracking.router)
+app.include_router(workout_session.router)
 
 populate_exercises()
