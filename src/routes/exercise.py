39b5ec4cc088f7c63bb936fc38709ca87b6084b8
annatvali <anntvaliashvili@gmail.com>
2025-02-04 14:40:29 +0400

    feat: create database seed script for predefined exercises

--- src/routes/exercise.py
+++ src/routes/exercise.py
@@ -1,41 +1,52 @@
-from fastapi import APIRouter, HTTPException
+from typing import List
 
-from src.models.exercise import Exercise
+from fastapi import APIRouter, Depends, HTTPException
+from sqlalchemy.orm import Session
+
+from src.models.exercise import Exercise as ExerciseModel
+from src.services.database import SessionLocal
 from src.services.exercise_db import add_exercise, delete_exercise, get_exercise_by_id, get_exercises, update_exercise
 
 router = APIRouter()
 
 
-@router.get("/exercises", response_model=list[Exercise])
-async def read_exercises():
-    return get_exercises()
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
 
 
-@router.post("/exercises", response_model=Exercise)
-async def create_exercise(exercise: Exercise):
-    add_exercise(exercise)
-    return exercise
+@router.get("/exercises", response_model=List[ExerciseModel])
+async def read_exercises(db: Session = Depends(get_db)):
+    return get_exercises(db)
 
 
-@router.get("/exercises/{exercise_id}", response_model=Exercise)
-async def read_exercise(exercise_id: int):
-    exercise = get_exercise_by_id(exercise_id)
-    if exercise is None:
+@router.post("/exercises", response_model=ExerciseModel)
+async def create_exercise(exercise: ExerciseModel, db: Session = Depends(get_db)):
+    return add_exercise(db, exercise)
+
+
+@router.get("/exercises/{exercise_id}", response_model=ExerciseModel)
+async def read_exercise(exercise_id: int, db: Session = Depends(get_db)):
+    db_exercise = get_exercise_by_id(db, exercise_id)
+    if db_exercise is None:
         raise HTTPException(status_code=404, detail="Exercise not found")
-    return exercise
+    return db_exercise
 
 
-@router.put("/exercises/{exercise_id}", response_model=Exercise)
-async def update_exercise_endpoint(exercise_id: int, updated_exercise: Exercise):
-    exercise = update_exercise(exercise_id, updated_exercise)
-    if exercise is None:
+@router.put("/exercises/{exercise_id}", response_model=ExerciseModel)
+async def update_exercise_endpoint(exercise_id: int, updated_exercise: ExerciseModel, db: Session = Depends(get_db)):
+    db_exercise = update_exercise(db, exercise_id, updated_exercise)
+    if db_exercise is None:
         raise HTTPException(status_code=404, detail="Exercise not found")
-    return exercise
+    return db_exercise
 
 
-@router.delete("/exercises/{exercise_id}", response_model=Exercise)
-async def delete_exercise_endpoint(exercise_id: int):
-    exercise = delete_exercise(exercise_id)
-    if exercise is None:
+@router.delete("/exercises/{exercise_id}", response_model=ExerciseModel)
+async def delete_exercise_endpoint(exercise_id: int, db: Session = Depends(get_db)):
+    db_exercise = delete_exercise(db, exercise_id)
+    if db_exercise is None:
         raise HTTPException(status_code=404, detail="Exercise not found")
-    return exercise
+    return db_exercise
