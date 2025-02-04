24471cf417e411d762b5106647774f78f9b20a9d
annatvali <anntvaliashvili@gmail.com>
2025-02-04 15:31:50 +0400

    feat: update main application to use SQLAlchemy and seed database

--- src/main.py
+++ src/main.py
@@ -1,14 +1,35 @@
+from contextlib import asynccontextmanager
+
 from fastapi import FastAPI
 
 from src.routes import auth, exercise, tracking, workout_plan, workout_session
-from src.services.populate_exercises import populate_exercises
+from src.services.database import Base, SessionLocal, engine
+from src.services.seed_database import seed_exercises
 
-app = FastAPI()
+
+@asynccontextmanager
+async def lifespan():
+    db = SessionLocal()
+    seed_exercises(db)
+    db.close()
+    yield
+
+
+app = FastAPI(lifespan=lifespan)
+
+Base.metadata.create_all(bind=engine)
+
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
 
 app.include_router(auth.router)
 app.include_router(exercise.router)
 app.include_router(workout_plan.router)
 app.include_router(tracking.router)
 app.include_router(workout_session.router)
-
-populate_exercises()
